6eba9191214563bf7f4dc404dc8aeeca561c36de
James Lockhart <james.lockhart117@gmail.com>
2018-02-19 12:28:32 +0300

    Initial commit, created duck class and initilized duck list

---
+++ Lists/Lists/Duck.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lists
+{
+
+    //use enum to keep track of what sort of ducks are in the collection
+    enum KindOfDuck
+    {
+        Mallard,
+        Muscovy,
+        Decoy
+    }
+
+    class Duck
+    {
+        public int Size;
+        public KindOfDuck Kind;
+    }
+}
---
+++ Lists/Lists/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //create a new list of duck objects
+            List<Duck> ducks = new List<Duck>()
+            {
+                new Duck(){Kind = KindOfDuck.Mallard, Size = 17},
+                new Duck(){Kind = KindOfDuck.Muscovy, Size = 18},
+                new Duck(){Kind = KindOfDuck.Decoy, Size = 14},
+                new Duck(){Kind = KindOfDuck.Mallard, Size = 11},
+                new Duck(){Kind = KindOfDuck.Muscovy, Size = 14},
+                new Duck(){Kind = KindOfDuck.Decoy, Size = 13},
+            };
+
+            //readkey is used to keep the console open until a key is pressed
+            Console.ReadKey();
+        }
+    }
+}
