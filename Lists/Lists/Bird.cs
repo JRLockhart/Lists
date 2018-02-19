c7a401b475a59e827007b76a9435d5d2e887930a
James Lockhart <james.lockhart117@gmail.com>
2018-02-19 13:33:55 +0300

    Added a base class Bird with basic properties

---
+++ Lists/Lists/Bird.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lists
+{
+    //base class bird
+    class Bird
+    {
+        //basic property
+        public string Name { get; set; }
+
+        //fly method to be overridden by various bird types
+        public virtual void Fly()
+        {
+            Console.WriteLine("Flap, Flap");
+        }
+
+        //the tostring method will make it easy to see which class is which
+        public override string ToString()
+        {
+            return "A bird named " + Name;
+        }
+    }
+}
